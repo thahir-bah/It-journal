94648bf0929f087f01192e3e987c1ce28b493d87
Aboubakary Abdourahmaan <aboubakayabdourahman@gmail.com>
2022-06-01 11:28:22 +0100

    initial commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello City</title>
+       
+    </head>
+    <body>
+     <h1>hello from Quebec!</h1>
+     <p>It's currently {{date('h:i A')}} .</p>
+
+     <footer>
+         <p>&copy; Copyrught {{date('Y')}}  &middot; <a href="#">About Us</a></p>
+     </footer>
+    </body>
+</html>
